f2ceff658418d5beafee334b767306cae6761817
Sandesh151 <87889995+Sandesh151@users.noreply.github.com>
2021-07-24 12:24:23 +0530

    Added Changes

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -9,6 +9,16 @@ namespace FileData
     {
         public static void Main(string[] args)
         {
+             var args1 = args[0];
+            var args2 = args[1];
+             
+            string v = FileDetails.Version(args2);
+            Console.Write(v);
+
+            string s = FileDetails.Version(args2);
+        
+            Console.Write(s);
+            Console.ReadKey();
         }
     }
 }
