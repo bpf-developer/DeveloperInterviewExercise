9c48431bd6af213d4405c0ce229285072dd4402b
nalawadebapusaheb <92287197+nalawadebapusaheb@users.noreply.github.com>
2021-10-11 10:37:05 +0530

    Update Program.cs
    Task1 completed.

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -9,6 +9,28 @@ namespace FileData
     {
         public static void Main(string[] args)
         {
+         args= new string[2];
+         string a;
+         string b;
+         for(int i=0;i<args.Length<2;i++)
+         {
+             if(i==0)
+                 a=args[i];
+             
+             if(i==1)
+              b=args[i];
+                       
+           
+         }
+            string result;
+            if(a=="-v")
+            {
+                FileDetails f= new FileDetails();
+                result= f.Version(b);
+                
+                Console.ReadLine(result);
+                
+            }
         }
     }
 }
